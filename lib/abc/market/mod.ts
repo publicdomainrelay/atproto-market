9774ff97cf17edff2e9db54857370e8d8f15cc99
John <johnandersen777@protonmail.com>
2026-06-17 01:44:17 -0700

    registry
    Signed-off-by: John <johnandersen777@protonmail.com>

--- lib/abc/market/mod.ts
+++ lib/abc/market/mod.ts
@@ -1,3 +1,4 @@
 export * from "./resolve.ts";
 export * from "./settlement.ts";
 export * from "./contract.ts";
+export * from "./registry.ts";
---
+++ lib/market-registry-atproto/mod.ts
@@ -0,0 +1,2 @@
+export * from "./registration-store.ts";
+export * from "./health-checker.ts";
